33a7551a18e694010b5a1eb3d1f5655c3e6d71ad
Diogo Viana <diogo.g.viana@gmail.com>
2024-04-03 23:39:54 +0100

    initial project setup

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import * as packageJson from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  build: {
+    minify: true,
+    sourcemap: true,
+    cssCodeSplit: false,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "@deadcow/ui",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+});
