e1a641905206162a3b3b321ce9b6583261aa95bb
Diogo Viana <diogo.g.viana@gmail.com>
2024-04-04 01:27:27 +0100

    wip storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,28 @@
 import type { Preview } from "@storybook/react";
-
+import "../src/styles.css";
+import "./index.css";
 const preview: Preview = {
   parameters: {
+    layout: "centered",
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    themes: {
+      clearable: false,
+      list: [
+        {
+          name: "Light",
+          class: ["light"],
+          default: true,
+        },
+        {
+          name: "Dark",
+          class: ["dark"],
+        },
+      ],
     },
   },
 };
